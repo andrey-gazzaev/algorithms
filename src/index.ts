af5c32679d107e405d09b7e23f7fb7e64c7c9d36
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2023-05-24 16:20:21 +0700

    Add index file for algorithm testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+import {heapSort} from './sorts/heapSort';
+import {generateSortedArray} from './utils/generateSotredArray';
+import {shuffle} from './utils/shuffle';
+import {timer} from './utils/timer';
+
+const sortedArray = generateSortedArray(100_000).reverse();
+const unsortedArray = shuffle([...sortedArray]);
+
+console.log(timer(() => heapSort(unsortedArray)));
