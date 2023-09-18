f4a92e4680bb9c3a1fb7a9ae65905ef3c96336dd
Andrey Gazzaev <andrey.gazzaev@saritasa.com>
2023-09-18 12:10:39 +0700

    Add queue

---
+++ src/structures/queue.ts
@@ -0,0 +1,27 @@
+import {List} from './list';
+
+export class Queue<T> {
+  private readonly set: List<T>;
+
+  /** Queue length. */
+  public get length() {
+    return this.set.toArray().length;
+  }
+
+  public constructor(...values: T[]) {
+    this.set = values.length > 0 ? new List(...values) : new List();
+  }
+
+  /** Adds an item to the end of the queue. */
+  public push(value: T): void {
+    this.set.push(value);
+  }
+
+  /**
+   * Deletes and returns an item from the beginning of the queue.
+   * If the queue is empty, null value is returned.
+   */
+  public pop(): T | null {
+    return this.set.pop();
+  }
+}
